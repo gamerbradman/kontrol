196b78653367f66f5be281951a55f8a186695db4
Ayush <ayushstillworks@gmail.com>
2026-04-17 14:11:15 +0530

    Implemented AI providers: -Added a "Test AI" button to trigger an AI job; shows a "AI Job queued" toast on success.
    -Introduced a new public API/mutation to queue AI execution.
    
    -Backend now processes AI tasks using multiple providers and returns aggregated results.
    
    -Added AI SDK dependencies to support new AI job execution.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,14 @@ const Page = () => {
     }
   }));
 
+  const testAi = useMutation(trpc.testAi.mutationOptions({
+    onSuccess:()=>{
+      toast.success("ai job queued")
+    }
+  }))
+
+
+
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
@@ -27,6 +35,9 @@ const Page = () => {
       <div>
       {JSON.stringify(data, null ,2)}
       </div>
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        Test AI
+      </Button>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         Create workflow
       </Button>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,25 +1,55 @@
 import {prisma} from "../lib/prisma";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" ,
-   triggers:{event: "test/hello.world"} },
+
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
+
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" ,
+   triggers:{event: "execute/ai"} },
   async ({ event, step }) => {
-    // Fetching the video
-    await step.sleep("fetching", "5s");
+     await step.sleep("pretend", "5s");
 
-    // Transcribing
-    await step.sleep("trancribing", "5s");
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant.",
+        prompt: "what is 2+2?",
+      }
+    );
+    const { steps: openaiSteps} = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-4o-mini"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2 + 2?",
+      }
+    );
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-haiku-4-5"),
+        system: "You are a helpful assistant.",
+        prompt: "What is 2 + 2?",
+      }
+    );
 
-    // Sending transcription to AI
-    await step.sleep("sending-to-ai", "5s");
 
-    await step.run("create-workflow", () => {
-      return prisma.workflow.create({
-        data: {
-          name: "workflow-from-inngest",
-        },
-      });
-    });
+      return {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps,
+    };
   },
 );
